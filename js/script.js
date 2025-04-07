9bbfb1b6ad31b47c9577e46b0df682d0f5c9d7e0
ZunairahKhan05 <khanzunairah05@gmail.com>
2025-04-07 15:33:30 +0100

    initial

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleThemeBtn = document.getElementById("toggleTheme");
+    toggleThemeBtn.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+    });
+})
