99972c07c3d4cd3b8fb99b272d98aebbdc67ed55
ZunairahKhan05 <khanzunairah05@gmail.com>
2025-04-14 22:34:48 +0100

    JS added to contact

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  //
+    //
     const toggleThemeBtn = document.getElementById("toggleTheme");
     toggleThemeBtn.addEventListener("click", () => {
       document.body.classList.toggle("dark-mode");
     });
+
+    
+    //
+    const contactForm = document.querySelector(".contact-form");
+    contactForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Thank you! Your message has been submitted.");
+      contactForm.reset();
+    });
+    
   
 })
