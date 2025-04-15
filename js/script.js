a534bc7e720752802130d77f19a5e92e0fe06263
ZunairahKhan05 <khanzunairah05@gmail.com>
2025-04-15 17:30:13 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleThemeBtn = document.getElementById("toggleTheme");
-    toggleThemeBtn.addEventListener("click", () => {
-      document.body.classList.toggle("dark-mode");
-    });
-})
