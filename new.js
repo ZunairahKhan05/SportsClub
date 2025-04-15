8a67df46419f51dc6c4a9528a457ffe7e1f00ab5
Sandip Kr Mahato <skmahato1822@gmail.com>
2025-04-15 16:58:09 +0100

    Add files via upload
    new javascript file

---
+++ new.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //
+    const toggleThemeBtn = document.getElementById("toggleTheme");
+    toggleThemeBtn.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+    });
+    
+  
+})
